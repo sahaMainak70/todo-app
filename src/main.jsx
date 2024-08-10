a506586fd6a6b76b88e67dce5fd9f4343f16acaf
sahaMainak70 <sahamainak70@gmail.com>
2024-08-10 13:20:24 +0530

    chore: Update main.jsx to use BrowserRouter for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(<App />);
